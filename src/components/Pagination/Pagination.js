3418b74d5b60a94b097f4c71edcceb9ff08fc1d6
Sara Peksin <peksin.sara@gmail.com>
2021-04-29 16:46:01 +0200

    pagination

---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import "./Pagination.css";
+import Pagination from '@material-ui/lab/Pagination';
+
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
+export default function CustomPagination({ setPage, numOfPages = 10 }) {
+  // Scroll to top when page changes
+  const handlePageChange = (page) => {
+    setPage(page);
+    window.scroll(0, 0);
+  };
+
+  return (
+    <div className="pagination">
+      <ThemeProvider theme={darkTheme}>
+        <Pagination
+          onChange={(e) => handlePageChange(e.target.textContent)}
+          count={numOfPages}
+          color="primary"
+
+        />
+      </ThemeProvider>
+    </div>
+  );
+}
--- src/pages/Series/Series.js
+++ src/pages/Series/Series.js
@@ -2,24 +2,30 @@ import React, { useState, useEffect } from "react";
 import "./Series.css";
 import axios from "axios";
 import SingleItem from  '../../components/SingleItem/SingleItem';
+import Pagination from "../../components/Pagination/Pagination";
 
 const Series = () => {
-    const [content, setContent] = useState([]);
-    const API_KEY = "e60e2f0cc6c14c63cb04b450da94ebfd";
+  const [content, setContent] = useState([]);
+  const API_KEY = "e60e2f0cc6c14c63cb04b450da94ebfd";
+  const [page, setPage] = useState(1);
 
   const fetchSeries = async () => {
     const { data } = await axios.get(
-      `https://api.themoviedb.org/3/tv/top_rated?api_key=${API_KEY}&language=en-US&page=1`
-      );
+      `https://api.themoviedb.org/3/tv/top_rated?api_key=${API_KEY}&language=en-US&page=&page=${page}`);
     
 
     console.log(data);
       setContent(data.results);
-    };
+  };
+    
 
     useEffect(() => {
       fetchSeries();
-    }, []);
+   
+    }, [page]);
+  
+    
+
 
 
   return (
@@ -33,7 +39,9 @@ const Series = () => {
                       <SingleItem key={c.id} id={c.id} poster={c.poster_path} description={c.overview}  popularity={c.popularity} name={c.name} vote_average={ c.vote_average}/>
                   ))}
 
-          </div>
+      </div>
+      <Pagination />
+      
     </div>
   );
 };
